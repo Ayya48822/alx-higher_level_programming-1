e308c00c86ac5aa7c8d5279b6631cf53f5228ea6
samsonkimani <samsonkimani43@gmail.com>
2022-12-05 11:12:19 +0300

    retrieve an element at a given index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,4 @@
 def element_at(my_list, idx):
     if idx < 0 and idx > len(my_list) - 1:
         return None
-    for i in range(idx):
-        element = my_list[i + 1]
-    return element
+    return my_list[idx]
