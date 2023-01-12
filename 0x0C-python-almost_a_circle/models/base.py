8500a2312087d27167557982a698933166181f91
samsonkimani <samsonkimani43@gmail.com>
2023-01-12 17:51:32 +0300

    created the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module for the base model"""
+
+
+class Base:
+    """ initializing the number of object variable"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ creating the init function
+
+        Args:
+            @id: the id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
