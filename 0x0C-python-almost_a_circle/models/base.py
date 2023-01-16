aabcd009d72f6de5a2680d56f886e3210594ea56
samsonkimani <samsonkimani43@gmail.com>
2023-01-16 18:51:17 +0300

    creating a list from json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,27 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file functions"""
+        filename = "{}.json".format(cls.__name__)
+        list_dict = []
+        
+        if not list_objs:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_dict.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dict)
+
+        with open(filename, 'w', encoding="utf-8") as my_file:
+            my_file.write(lists)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return list from json string"""
+        if not json_string:
+            return []
+        return json.loads(json_string)
