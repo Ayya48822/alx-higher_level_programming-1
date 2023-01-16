d7a6b3fc92bd003c36b8b190197007e02bfeb222
samsonkimani <samsonkimani43@gmail.com>
2023-01-16 13:36:11 +0300

    creating the size public setter and getter methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,26 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initializing the instance"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ __str__ magic method"""
+        square = "[Square] "
+        square_id = "({}) ".format(self.id)
+        square_xy = "{}/{} - ".format(self.x, self.y)
+        square_size = "{}".format(self.width)
+
+        return square + square_id + square_xy + square_size
+
+    @property
+    def size(self):
+        """getting the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setting the value of the size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ creating the update function"""
